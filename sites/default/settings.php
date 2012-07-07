60babf1468895114d4948724801c3301485c355f
jeissonp <jeissonp@gmail.com>
2012-07-07 15:01:14 -0500

    settings

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -200,7 +200,21 @@
  *   );
  * @endcode
  */
-$databases = array();
+$databases = array (
+  'default' => 
+  array (
+    'default' => 
+    array (
+      'database' => 'ves',
+      'username' => 'ves',
+      'password' => '12345678',
+      'host' => 'localhost',
+      'port' => '',
+      'driver' => 'mysql',
+      'prefix' => '',
+    ),
+  ),
+);
 
 /**
  * Access control for update.php script.
@@ -232,7 +246,7 @@ $update_free_access = FALSE;
  *   $drupal_hash_salt = file_get_contents('/home/example/salt.txt');
  *
  */
-$drupal_hash_salt = '';
+$drupal_hash_salt = 'uCermTF5qv8HsOf2JNUemdu2xp3AnLvENtAt3YH6DGM';
 
 /**
  * Base URL (optional).
