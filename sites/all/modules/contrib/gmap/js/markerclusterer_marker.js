a60ee41f556d5ec83a9fe492a1658c1cad89bd11
josmera <josmera01@gmail.com>
2012-07-07 16:17:46 -0500

    Modulos

---
+++ sites/all/modules/contrib/gmap/js/markerclusterer_marker.js
@@ -0,0 +1,37 @@
+
+/**
+ * @file
+ * GMap Markers
+ * Gmaps Utility Library MarkerClusterer API version
+ */
+
+/*global Drupal, GMarker, MarkerClusterer */
+
+// Replace to override marker creation
+Drupal.gmap.factory.marker = function (opts) {
+  return new google.maps.Marker(opts);
+};
+
+Drupal.gmap.addHandler('gmap', function (elem) {
+  var obj = this;
+
+  obj.bind('init', function () {
+    // Set up the markermanager.
+	obj.mc = new MarkerClusterer(obj.map, [], {
+	  maxZoom: parseInt(Drupal.settings.gmap_markermanager["maxZoom"]),
+	  gridSize: parseInt(Drupal.settings.gmap_markermanager["gridSize"])
+	});
+  });
+  obj.bind('addmarker', function (marker) {
+    // @@@ Would be really nice to have bulk adding support in gmap.
+    obj.mc.addMarkers([marker.marker]);
+  });
+
+  obj.bind('delmarker', function (marker) {
+    obj.mc.removeMarker(marker.marker);
+  });
+
+  obj.bind('clearmarkers', function () {
+    obj.mc.clearMarkers();
+  });
+});
